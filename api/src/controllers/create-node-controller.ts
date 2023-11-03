10399d5129b386346cdae45a1d6b72f71a8dbd2c
abinash <abinashphulkonwar1@gmail.com>
2023-11-03 22:45:51 +0530

    add create node handler

---
+++ api/src/controllers/create-node-controller.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from "express";
+import { Nodedb } from "../db/node";
+
+export const CreateNode = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const node = Nodedb.build(req.body);
+  //  await node.save();
+  res.json(node);
+};
